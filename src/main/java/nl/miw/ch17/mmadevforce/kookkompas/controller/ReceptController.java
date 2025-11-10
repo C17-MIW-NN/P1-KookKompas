b77aee166e447ccc1c1197f0b8cb54535754225b
Clampiee <zijlstaarjen@hotmail.com>
2025-11-10 12:01:17 +0100

    packages updated

---
+++ src/main/java/nl/miw/ch17/mmadevforce/kookkompas/controller/ReceptController.java
@@ -0,0 +1,8 @@
+package nl.miw.ch17.mmadevforce.kookkompas.controller;
+
+/**
+ * @author Arjen Zijlstra
+ *
+ */
+public class ReceptController {
+}
---
+++ src/main/java/nl/miw/ch17/mmadevforce/kookkompas/repositories/ReceptRepository.java
@@ -0,0 +1,8 @@
+package nl.miw.ch17.mmadevforce.kookkompas.repositories;
+
+/**
+ * @author Arjen Zijlstra
+ *
+ */
+public interface ReceptRepository {
+}
