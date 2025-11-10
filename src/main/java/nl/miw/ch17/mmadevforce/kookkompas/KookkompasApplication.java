9d73de079de634cb84e6d87e43eb17e0b6a3535c
Manon Kuipers <manon.kuipers2103@gmail.com>
2025-11-10 11:26:25 +0100

    first commit

---
+++ src/main/java/nl/miw/ch17/mmadevforce/kookkompas/KookkompasApplication.java
@@ -0,0 +1,13 @@
+package nl.miw.ch17.mmadevforce.kookkompas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KookkompasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KookkompasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miw/ch17/mmadevforce/kookkompas/KookkompasApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miw.ch17.mmadevforce.kookkompas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KookkompasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
