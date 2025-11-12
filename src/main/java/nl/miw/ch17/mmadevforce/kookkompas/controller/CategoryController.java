1708bc93e31cfd8dab8b1f7755c42f3a7eeced0e
Strdr-rgb <mel.vandervlies@gmail.com>
2025-11-12 10:57:06 +0100

    Created new files to fill later

---
+++ src/main/java/nl/miw/ch17/mmadevforce/kookkompas/controller/CategoryController.java
@@ -0,0 +1,7 @@
+package nl.miw.ch17.mmadevforce.kookkompas.controller;
+
+/**
+  * @author Melanie van der Vlies
+  * Doel van de class:
+  */public class CategoryController {
+}
---
+++ src/main/java/nl/miw/ch17/mmadevforce/kookkompas/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package nl.miw.ch17.mmadevforce.kookkompas.repositories;
+
+/**
+  * @author Melanie van der Vlies
+  * Doel van de class:
+  */public interface CategoryRepository {
+}
