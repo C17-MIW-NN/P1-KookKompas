2363895d4e169ccdefb4c2b57c9e3537021bca49
Manon Kuipers <manon.kuipers2103@gmail.com>
2025-11-18 14:31:13 +0100

    checked maintainability in IngredientController and made IngredientService

--- src/main/java/nl/miw/ch17/mmadevforce/kookkompas/controller/IngredientController.java
+++ src/main/java/nl/miw/ch17/mmadevforce/kookkompas/controller/IngredientController.java
@@ -1,76 +1,64 @@
 package nl.miw.ch17.mmadevforce.kookkompas.controller;
 
 import nl.miw.ch17.mmadevforce.kookkompas.model.Ingredient;
-import nl.miw.ch17.mmadevforce.kookkompas.model.Recipe;
-import nl.miw.ch17.mmadevforce.kookkompas.repositories.IngredientRepository;
-import nl.miw.ch17.mmadevforce.kookkompas.repositories.RecipeRepository;
+import nl.miw.ch17.mmadevforce.kookkompas.service.IngredientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
  * @author MMA Dev Force
- * Doel methode
+ * Handles requests regarding ingredients
  */
 @Controller
 @RequestMapping("/ingredient")
 public class IngredientController {
+    private final IngredientService ingredientService;
 
-    private final IngredientRepository ingredientRepository;
-    private final RecipeRepository recipeRepository;
-
-    public IngredientController(IngredientRepository ingredientRepository, RecipeRepository recipeRepository) {
-        this.ingredientRepository = ingredientRepository;
-        this.recipeRepository = recipeRepository;
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/all")
     public String showIngredientOverview(Model datamodel) {
-        datamodel.addAttribute("ingredients", ingredientRepository.findAll());
+        datamodel.addAttribute("ingredients", ingredientService.findAllIngredients());
         return "ingredientOverview";
     }
 
     @GetMapping("/add")
     public String showIngredientForm(Model datamodel){
         datamodel.addAttribute("formIngredient", new Ingredient());
-
         return "ingredientForm";
     }
 
     @PostMapping("/save")
     public String saveIngredient(@ModelAttribute("formIngredient") Ingredient ingredientToBeSaved){
-        ingredientRepository.save(ingredientToBeSaved);
-        return "redirect:/ingredient/all";
+        ingredientService.saveIngredient(ingredientToBeSaved);
+        return getRedirectIngredientAll();
     }
+
 
     @GetMapping("/edit/{name}")
     public String showEditIngredientForm(@PathVariable("name") String name, Model datamodel) {
-        Optional<Ingredient> optionalIngredient = ingredientRepository.findByName(name);
+        Optional<Ingredient> optionalIngredient = ingredientService.findByIngredientName(name);
 
         if (optionalIngredient.isPresent()) {
             datamodel.addAttribute("formIngredient", optionalIngredient.get());
             return "ingredientForm";
         }
 
-        return "redirect:/ingredient/all";
+        return getRedirectIngredientAll();
     }
 
     @GetMapping("/delete/{ingredientId}")
     public String deleteIngredient(@PathVariable("ingredientId") Long ingredientId) {
-        Ingredient ingredient = ingredientRepository.findById(ingredientId).orElse(null);
-        if (ingredient != null) {
-            List<Recipe> recipesWithIngredient = recipeRepository.findByRecipeingredientsIngredient(ingredient);
-            recipesWithIngredient.forEach(recipe -> {
-                recipe.getRecipeingredients()
-                        .removeIf(recipeIngredient ->
-                                recipeIngredient.getIngredient().getIngredientId()
-                                        .equals(ingredient.getIngredientId()));
-            });
-            ingredientRepository.delete(ingredient);
-        }
+        ingredientService.deleteIngredient(ingredientId);
+        return getRedirectIngredientAll();
+    }
+
+    private static String getRedirectIngredientAll() {
         return "redirect:/ingredient/all";
     }
 
---
+++ src/main/java/nl/miw/ch17/mmadevforce/kookkompas/service/IngredientService.java
@@ -0,0 +1,51 @@
+package nl.miw.ch17.mmadevforce.kookkompas.service;
+
+import nl.miw.ch17.mmadevforce.kookkompas.model.Ingredient;
+import nl.miw.ch17.mmadevforce.kookkompas.model.Recipe;
+import nl.miw.ch17.mmadevforce.kookkompas.repositories.IngredientRepository;
+import nl.miw.ch17.mmadevforce.kookkompas.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author MMA Dev Force
+ * Managing functionalities from ingredients
+ */
+@Service
+public class IngredientService {
+    private final IngredientRepository ingredientRepository;
+    private final RecipeRepository recipeRepository;
+
+    public IngredientService(IngredientRepository ingredientRepository, RecipeRepository recipeRepository) {
+        this.ingredientRepository = ingredientRepository;
+        this.recipeRepository = recipeRepository;
+    }
+
+    public List<Ingredient> findAllIngredients() {
+        return ingredientRepository.findAll();
+    }
+
+    public Ingredient saveIngredient(Ingredient ingredient) {
+        return ingredientRepository.save(ingredient);
+    }
+
+    public Optional<Ingredient> findByIngredientName(String name) {
+        return ingredientRepository.findByName(name);
+    }
+    public void deleteIngredient(Long ingredientId) {
+        Ingredient ingredient = ingredientRepository.findById(ingredientId).orElse(null);
+
+        if (ingredient != null) {
+            List<Recipe> recipesWithIngredient = recipeRepository.findByRecipeingredientsIngredient(ingredient);
+            recipesWithIngredient.forEach(recipe -> {
+                recipe.getRecipeingredients()
+                        .removeIf(recipeIngredient ->
+                                recipeIngredient.getIngredient().getIngredientId()
+                                        .equals(ingredient.getIngredientId()));
+            });
+            ingredientRepository.delete(ingredient);
+        }
+    }
+}
